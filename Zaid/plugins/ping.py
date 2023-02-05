05be2c6e36f07cb1a93844f3d88ddb5fc1558986
Akshay Kumar S B <jinmori58690@gmail.com>
2023-02-06 01:33:58 +0530

    Update ping.py

--- Zaid/plugins/ping.py
+++ Zaid/plugins/ping.py
@@ -36,5 +36,5 @@ async def _(event):
    delta_ping = time() - start
    uptime_sec = (current_time - START_TIME).total_seconds()
    uptime = await _human_time_duration(int(uptime_sec))
-   UMM = [[Button.url("⚜ Cԋαɳɳҽʅ ⚜", "https://t.me/TheUpdatesChannel")]]
-   await event.reply(f"╰☞ 𝗣𝗢𝗡𝗚™╮\n☞ {delta_ping * 1000:.3f}\n☞ {uptime}", buttons=UMM)
+   UMM = [[Button.url("Maintained By", "https://t.me/Walhalla_Network")]]
+   await event.reply(f"♡ 𝗣𝗢𝗡𝗚\n ♡{delta_ping * 1000:.3f}\n♡ {uptime}", buttons=UMM)
